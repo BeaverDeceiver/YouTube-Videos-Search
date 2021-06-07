434dba827f8c4496f155d7b3b767e2ab91049c3f
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-07 10:25:52 +0300

    feat/secure api key in env file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyB14DKGO5IjpR6XFqeuFhZbuktDa1F5lho';
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: ' https://www.googleapis.com/youtube/v3/',
