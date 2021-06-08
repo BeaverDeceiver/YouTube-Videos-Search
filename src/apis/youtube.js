b36b6661dddbd8b2e0733dba03d6535d866eba2a
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-08 11:39:07 +0300

    feat/update api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { SEARCH_PART_SNIPPET } from '../constants/searchConstants';
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: ' https://www.googleapis.com/youtube/v3/',
   params: {
-    part: 'snippet',
+    part: SEARCH_PART_SNIPPET,
     key: API_KEY,
   },
 });
